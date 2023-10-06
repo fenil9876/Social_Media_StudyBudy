184ce497c29667ab4eeca9df5535b03e5c4b5daf
fenil9876 <89098475+fenil9876@users.noreply.github.com>
2023-10-06 11:50:15 +0530

    Update settings.py

--- studybud/settings.py
+++ studybud/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+//static files
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
